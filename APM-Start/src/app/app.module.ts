8484d7debddf16f2d86c5368eb651a13ff2c638d
Son Hoang <hoangthaison985@gmail.com>
2017-10-08 00:33:54 +0700

    finish module 3: Routing basic

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
@@ -19,6 +20,11 @@ import { MessageModule } from './messages/message.module';
   imports: [
     BrowserModule,
     HttpModule,
+    RouterModule.forRoot([
+      { path: 'welcome', component: WelcomeComponent },  
+      { path: '', redirectTo: 'welcome', pathMatch: 'full'},  
+      { path: '**', component: PageNotFoundComponent }  
+    ]),
     InMemoryWebApiModule.forRoot(ProductData, { delay: 1000 }),
     ProductModule,
     UserModule,
