cf34c8326de1a0831dc8ea9ef11795b968cf2186
Son Hoang <hoangthaison985@gmail.com>
2017-10-08 19:44:54 +0700

    finish Route Parameters

--- APM-Start/src/app/products/product-edit.component.ts
+++ APM-Start/src/app/products/product-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { MessageService } from '../messages/message.service';
 
@@ -6,68 +7,80 @@ import { IProduct } from './product';
 import { ProductService } from './product.service';
 
 @Component({
-    templateUrl: './app/products/product-edit.component.html',
-    styleUrls: ['./app/products/product-edit.component.css']
+  templateUrl: './app/products/product-edit.component.html',
+  styleUrls: ['./app/products/product-edit.component.css']
 })
-export class ProductEditComponent {
-    pageTitle: string = 'Product Edit';
-    errorMessage: string;
+export class ProductEditComponent implements OnInit {
+  pageTitle: string = 'Product Edit';
+  errorMessage: string;
 
-    product: IProduct;
+  product: IProduct;
 
-    constructor(private productService: ProductService,
-                private messageService: MessageService) { }
+  constructor(private productService: ProductService,
+              private messageService: MessageService,
+              private route: ActivatedRoute,
+              private router: Router) { }
 
-    getProduct(id: number): void {
-        this.productService.getProduct(id)
-            .subscribe(
-                (product: IProduct) => this.onProductRetrieved(product),
-                (error: any) => this.errorMessage = <any>error
-            );
+  ngOnInit(): void {
+    this.route.params.subscribe(
+      params => {
+        let id = +params['id'];
+        this.getProduct(id);
+      }
+    )
+  }
+
+  getProduct(id: number): void {
+    this.productService.getProduct(id)
+      .subscribe(
+      (product: IProduct) => this.onProductRetrieved(product),
+      (error: any) => this.errorMessage = <any>error
+      );
+  }
+
+  onProductRetrieved(product: IProduct): void {
+    this.product = product;
+
+    if (this.product.id === 0) {
+      this.pageTitle = 'Add Product';
+    } else {
+      this.pageTitle = `Edit Product: ${this.product.productName}`;
+    }
+  }
+
+  deleteProduct(): void {
+    if (this.product.id === 0) {
+      // Don't delete, it was never saved.
+      this.onSaveComplete();
+    } else {
+      if (confirm(`Really delete the product: ${this.product.productName}?`)) {
+        this.productService.deleteProduct(this.product.id)
+          .subscribe(
+          () => this.onSaveComplete(`${this.product.productName} was deleted`),
+          (error: any) => this.errorMessage = <any>error
+          );
+      }
+    }
+  }
+
+  saveProduct(): void {
+    if (true === true) {
+      this.productService.saveProduct(this.product)
+        .subscribe(
+        () => this.onSaveComplete(`${this.product.productName} was saved`),
+        (error: any) => this.errorMessage = <any>error
+        );
+    } else {
+      this.errorMessage = 'Please correct the validation errors.';
+    }
+  }
+
+  onSaveComplete(message?: string): void {
+    if (message) {
+      this.messageService.addMessage(message);
     }
 
-    onProductRetrieved(product: IProduct): void {
-        this.product = product;
-
-        if (this.product.id === 0) {
-            this.pageTitle = 'Add Product';
-        } else {
-            this.pageTitle = `Edit Product: ${this.product.productName}`;
-        }
-    }
-
-    deleteProduct(): void {
-        if (this.product.id === 0) {
-            // Don't delete, it was never saved.
-            this.onSaveComplete();
-       } else {
-            if (confirm(`Really delete the product: ${this.product.productName}?`)) {
-                this.productService.deleteProduct(this.product.id)
-                    .subscribe(
-                        () => this.onSaveComplete(`${this.product.productName} was deleted`),
-                        (error: any) => this.errorMessage = <any>error
-                    );
-            }
-        }
-    }
-
-    saveProduct(): void {
-        if (true === true) {
-            this.productService.saveProduct(this.product)
-                .subscribe(
-                    () => this.onSaveComplete(`${this.product.productName} was saved`),
-                    (error: any) => this.errorMessage = <any>error
-                );
-        } else {
-            this.errorMessage = 'Please correct the validation errors.';
-        }
-    }
-
-    onSaveComplete(message?: string): void {
-        if (message) {
-            this.messageService.addMessage(message);
-        }
-
-        // Navigate back to the product list
-    }
+    // Navigate back to the product list
+    this.router.navigate(['/products']);
+  }
 }
